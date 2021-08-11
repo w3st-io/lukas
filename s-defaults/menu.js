72f96bc05a742a211f169ce4a850cf65f1fb5c4a
aleem-ahmed <opall.io2020@gmail.com>
2021-08-11 17:56:08 -0400

    Improved

--- s-defaults/menu.js
+++ s-defaults/menu.js
@@ -3,22 +3,395 @@ module.exports = [
 		category: 'Platos Tipicos',
 		items: [
 			{
-				id: '',
-				name: 'hornado',
-				description: '',
-				price: 0.00,
-
+				name: "Hornado",
+				price: "$10.25",
+				description: "Roasted pork, rice, hominy corn & potato patties"
 			},
+			{
+				name: "Yapingacho",
+				price: "$13.00",
+				description: "rice, hominy corn, fried pork chunks, potato patties, sausage & eggs"
+			},
+			{
+				name: "Chaulafan",
+				price: "$12.00",
+				description: "mixed friend rice/roasted pork, hen, shrimp & sweet plantain"
+			},
+			{
+				name: "Guatita",
+				price: "$10.25",
+				description: "tripe in peanut butter & potatoes sauce, avocado & egg"
+			},
+			{
+				name: "Bandera Regula",
+				price: "$12.00",
+				description: "Bandera Regula"
+			},
+			{
+				name: "Fritado",
+				price: "$11.00",
+				description: "Fritado"
+			},
+			{
+				name: "Chuleta Frita",
+				price: "$10.25",
+				description: "Chuleta Frita"
+			},
+			{
+				name: "Mote Pillo con Carne Asada",
+				price: "$11.50",
+				description: "Hominy corn & steak"
+			},
+			{
+				name: "Bandera Serrana",
+				price: "$12.00",
+				description: "con pollo u hornado"
+			},
+			{
+				name: "Sancocho Ecuatoriano",
+				price: "$9.00",
+				description: "Pork ribs, rice, hominy corn & potato patties"
+			}
 		]
 	},
 	{
-		category: 'category 2',
+		category: 'Carnes/Beef',
 		items: [
 			{
-				name: 'product',
-				description: '',
-				price: 0.00
+				name: "Baby Churraso",
+				price: "$13.50",
+				description: "Skirt steak"
 			},
+			{
+				name: "Churrasco Bistek",
+				price: "$13.00",
+				description: "Churrasco Bistek"
+			},
+			{
+				name: "Churrasco",
+				price: "$12.00",
+				description: "Steak with rice, eggs, french fries & avocado"
+			},
+			{
+				name: "Carne Asada",
+				price: "$10.25",
+				description: "Carne Asada"
+			},
+			{
+				name: "Bistec",
+				price: "$11.50",
+				description: "Steak with rice & beans"
+			},
+			{
+				name: "Seco de Carne",
+				price: "$9.00",
+				description: "Beef strew with rice & beans"
+			},
+			{
+				name: "Seco de Chivo",
+				price: "$11.00",
+				description: "Goat stew"
+			},
+			{
+				name: "Lomito Saltado",
+				price: "$11.00",
+				description: "Pepper steak with rice, french fries, egg & avocado"
+			},
+			{
+				name: "Plato Luka",
+				price: "$12.50",
+				description: "Luka's plate"
+			},
+			{
+				name: "Plato Paisa",
+				price: "$12.50",
+				description: "Plato plate"
+			},
+			{
+				name: "Especial del Chef",
+				price: "$14.00",
+				description: "Chef's special"
+			},
+			{
+				name: "Menestra",
+				price: "$12.00",
+				description: "Grilled steak with rice, beans & sweet plantain"
+			}
+		]
+	},
+	{
+		category: 'Delicias Del Mas/Seafood',
+		items: [
+			{
+				name: "Filete Apanado",
+				price: "$10.50",
+				description: "Breaded fish fillet"
+			},
+			{
+				name: "Camarones Apanados",
+				price: "$13.00",
+				description: "Breaded shrimps"
+			},
+			{
+				name: "Arroz con Mariscos",
+				price: "M/P",
+				description: "Mixed seafood yellow rice"
+			},
+			{
+				name: "Arroz con Camarones",
+				price: "$13.50",
+				description: "Yellow rice with shrimps"
+			},
+			{
+				name: "Trucha",
+				price: "$13.00",
+				description: "Trout with rice, friend plantain & french fries"
+			},
+			{
+				name: "Filete de Pescado Frito",
+				price: "$10.50",
+				description: "Fried fish fillet"
+			},
+			{
+				name: "Camarones Al Ajillo",
+				price: "$13.00",
+				description: "Shrimps scampi, with rice & fried plantain"
+			},
+			{
+				name: "Camarones a la Francesa",
+				price: "$13.00",
+				description: "Shrimp franchise"
+			},
+			{
+				name: "Corvina",
+				price: "$13.25",
+				description: "Fish with rice, sweet plantain & french fries"
+			},
+			{
+				name: "Mar y Tierra",
+				price: "$15.00",
+				description: "Shrimp and beef"
+			},
+			{
+				name: "Corvina Sudada",
+				price: "$14.00",
+				description: "Corvina Sudada"
+			}
+		]
+	},
+	{
+		category: 'Pollos/Chicken',
+		items: [
+			{
+				name: "Seco de Pollo",
+				price: "$9.00",
+				description: "Chicken stew"
+			},
+			{
+				name: "Pechuga de Pollo a la Plancha",
+				price: "$9.50",
+				description: "Grilled chicken breast"
+			},
+			{
+				name: "Allitas de Pollo",
+				price: "$8.00",
+				description: "Chicken wings"
+			},
+			{
+				name: "Pollo a la Francesa",
+				price: "$10.00",
+				description: "Chicken Francaise"
+			},
+			{
+				name: "Pollo Frito",
+				price: "$8.50",
+				description: "Fried chicken with rice & beans"
+			},
+			{
+				name: "Pollo Apanado",
+				price: "$9.00",
+				description: "Breaded chicken cutlet"
+			},
+			{
+				name: "Quesadilla de Pollo Dos Amigos",
+				price: "$9.50",
+				description: "Chicken quesadillas"
+			},
+			{
+				name: "Dos Amigos",
+				price: "$11.00",
+				description: ""
+			}
+		]
+	},
+	{
+		category: 'Sopas/Soups',
+		items: [
+			{
+				name: "Gallino Soup",
+				price: "$8.25",
+				description: "Hen soup"
+			},
+			{
+				name: "Bagre Cat Fish Soup",
+				price: "$8.25",
+				description: "Cat fish soup"
+			},
+			{
+				name: "Bola Plantain",
+				price: "$8.75",
+				description: "Ball Soup"
+			},
+			{
+				name: "Pata Cow Feet Soup",
+				price: "$8.25",
+				description: "Cow feet soup"
+			}
+		]
+	},
+	{
+		category: 'A la Casta',
+		items: [
+			{
+				name: "Encebollado Mixto",
+				price: "$14.00",
+				description: "Encebollado mixto"
+			},
+			{
+				name: "Encebollado de Pescado",
+				price: "$10.50",
+				description: "Fish soup with cassava & onions"
+			},
+			{
+				name: "Sope de Mariscos PM",
+				price: "M/P",
+				description: "Seafood soup M/P"
+			},
+			{
+				name: "Ceviche",
+				price: "$7.75",
+				description: "Ceviche"
+			},
+			{
+				name: "Balserito",
+				price: "$15.00",
+				description: "Balserito"
+			},
+			{
+				name: "Salchipollo",
+				price: "$9.50",
+				description: "Chicken with french fries"
+			},
+			{
+				name: "Cheeseburger",
+				price: "$7.75",
+				description: "Cheeseburger deluxe"
+			},
+			{
+				name: "Salchipapas",
+				price: "$6.50",
+				description: "Hot dog with french fries"
+			}
+		]
+	},
+	{
+		category: 'Sandwiches & Plates',
+		items: [
+			{
+				name: "Cheesesteak Sandwich",
+				price: "$7.00",
+				description: "Peppers, onions"
+			},
+			{
+				name: "Chicken Sandwich",
+				price: "$6.00",
+				description: ""
+			},
+			{
+				name: "Chicken Calet Sandwich",
+				price: "$6.00",
+				description: ""
+			},
+			{
+				name: "Fish Sandwich",
+				price: "$8.00",
+				description: ""
+			},
+			{
+				name: "Bacon Egg Cheese",
+				price: "$4.50",
+				description: ""
+			},
+			{
+				name: "Ham Egg Cheese",
+				price: "$4.50",
+				description: ""
+			},
+			{
+				name: "Customized Omlete",
+				price: "?",
+				description: ""
+			},
+			{
+				name: "Grilled Chicken Salad",
+				price: "$8.00",
+				description: ""
+			},
+			{
+				name: "Quesadilla de Carne",
+				price: "$9.00",
+				description: ""
+			},
+			{
+				name: "Chicken Nuggets with French Fries",
+				price: "$6.00",
+				description: ""
+			}
+		]
+	},
+	{
+		category: 'Sides',
+		items: [
+			{
+				name: "Tostones",
+				price: "$4.00",
+				description: "Fried green eggplants"
+			},
+			{
+				name: "Maduros",
+				price: "$4.00",
+				description: "Maduros"
+			},
+			{
+				name: "Arroz",
+				price: "$3.50",
+				description: "Rice"
+			},
+			{
+				name: "Frijoles",
+				price: "$3.50",
+				description: "Beans"
+			},
+			{
+				name: "Papas Fritas",
+				price: "$3.50",
+				description: "French Fries"
+			},
+			{
+				name: "Chicharron",
+				price: "$3.50",
+				description: "Fried pork rinds"
+			},
+			{
+				name: "Chorizo",
+				price: "$3.25",
+				description: "Chorizo"
+			},
+			{
+				name: "Mote",
+				price: "$4.00",
+				description: "Motes"
+			}
 		]
 	},
 ]
